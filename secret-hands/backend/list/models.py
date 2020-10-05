cc05eb8e64a39befb58883e8b130627cbe14caa1
yunwoon <chea2089@naver.com>
2020-10-05 19:15:20 +0900

    react only

--- secret-hands/backend/list/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class List(models.Model):
-    title = models.CharField(max_length=20)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.title
--- secret-hands/backend/list/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ListList.as_view()),
-    path('<int:pk>/', views.DetailList.as_view()),
-]
